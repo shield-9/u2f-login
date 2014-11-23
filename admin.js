74a63ab13a0fe0b8cc49f55883e748afb201161e
Daisuke Takahashi <daisuke@extendwings.com>
2014-11-23 15:58:33 +0900

    Use setTimeout()

--- admin.js
+++ admin.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
-	$("#u2f-register").click(function() {
-		$( this ).text('Now insert (and tap) your Security Key');
+	$u2f_reg = $("#u2f-register");
+	$u2f_reg.click(function() {
+		setTimeout(function() {
+			var request = {"challenge": "CR-2c5Pq9GkgIG9IwAiPLa2jLaXNXlIRY858gbY3zKE", "version": "U2F_V2", "appId": "https://www.extendwings.com"};
+			console.log("Register: ", request);
+			$u2f_reg.text('Now insert (and tap) your Security Key');
+
+/*
+			u2f.register([request], [], function(data) {
+				console.log("Register callback", data);
+				$('#bind-data').val(JSON.stringify(data));
+				$('#bind-form').submit();
+			});
+*/
+		}, 1000);
 	});
 });
